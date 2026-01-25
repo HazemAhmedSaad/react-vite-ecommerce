385bfa7dd2620c03295f1f21447572cff8d77780
HazemAhmedSaad <zozahmed939@gmail.com>
2026-01-25 17:09:57 +0200

    update name of import file to work on vercal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
-import Home from "./components/Home/Home";
+import Home from './components/Home/Home';
 
 function App() {
   const [count, setCount] = useState(0);
