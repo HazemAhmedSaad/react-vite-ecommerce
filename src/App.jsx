c7316a436abf288c9ac844f44743be0fc0a3d2b4
HazemAhmedSaad <zozahmed939@gmail.com>
2026-02-16 04:33:37 +0200

    get products from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./components/Router/Router";
-import { AuthProvider } from './context/AuthenticationToken';
+import { AuthProvider } from "./context/AuthenticationToken";
 import { ThemeProvider } from "./context/ThemeContext";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 function App() {
+  const queryClient = new QueryClient();
   return (
+    
     <div>
       <AuthProvider>
         <ThemeProvider>
-        <RouterProvider router={router} />
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+          </QueryClientProvider>
         </ThemeProvider>
       </AuthProvider>
     </div>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,63 +1,111 @@
 import { useState } from "react";
-import { Button, ListGroup, Container } from "react-bootstrap";
+import { ListGroup, Container } from "react-bootstrap";
 import "./Products.css";
-
+import product from "../../assets/images/product.jpg";
+import HashLoader from "./../../../node_modules/react-spinners/esm/HashLoader";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 export default function Products() {
   const [open, setOpen] = useState(false);
+  const getAllProducts = () =>
+    axios.get("https://ecommerce.routemisr.com/api/v1/products");
+
+  const { data, isLoading, isError, isFetching } = useQuery({
+    queryKey: ["allProducts"],
+    queryFn: getAllProducts,
+  });
+
+  console.log(data?.data.data);
 
   const handleToggle = () => {
     setOpen((prev) => !prev);
   };
+
+  if (isLoading) {
+    return (
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <HashLoader color="#f3a909" size={75} />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <p>Error loading products</p>;
+  }
 
   return (
     <div className="products-wrapper">
       <div className="products-content">
         {/* Sidebar */}
         <div className={`  sidebar ${open ? "open" : ""}`}>
-          <div className="side-fixed">
+          <div className="">
             {open && (
               <div className="sidebar-inner">
-                <Button variant="light" onClick={handleToggle} className="mb-3">
+                <span
+                  variant="light"
+                  onClick={handleToggle}
+                  className="mb-3 tog-close-btn "
+                >
                   <i className="fa-solid fa-angles-left"></i>
-                </Button>
+                </span>
+                <h2>heloooooo</h2>
                 <hr />
-
-                <ListGroup variant="flush">
-                  {["Inbox", "Starred", "Send email", "Drafts"].map((item) => (
-                    <ListGroup.Item action key={item}>
-                      {item}
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-
-                <hr />
-
-                <ListGroup variant="flush">
-                  {["All mail", "Trash", "Spam", "More", "Trash", "Spam"].map((item) => (
-                    <ListGroup.Item action key={item}>
-                      {item}
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
+                <ul>
+                  <li>fffffffffffffff</li>
+                  <li>fffffffffffffff</li>
+                  <li>fffffffffffffff</li>
+                  <li>fffffffffffffffffffffffffffffffffffffff</li>
+                </ul>
               </div>
             )}
           </div>
         </div>
 
         {/* Main Content */}
-        <Container fluid className="main-content container">
-          {!open && (
-            <span variant="" onClick={handleToggle} className="toggle-btn ">
-              <i className="fa-solid fa-angles-right"></i>
-            </span>
-          )}
-          <h1>Hazem</h1>
-          <p>Welcome to Products Page</p>
-          <div>Scroll Content Example</div> <h1>Hazem</h1>
-          <p>Welcome to Products Page</p>
-          <div>Scroll Content Example</div> <h1>Hazem</h1>
+        <div className="main-content container">
+          <div>
+            {!open && (
+              <span variant="" onClick={handleToggle} className="toggle-btn ">
+                <i className="fa-solid fa-angles-right"></i>
+              </span>
+            )}
+            <div className="row row-cols-auto gap-4 justify-content-around">
+              {data?.data.data?.map((product) => (
+                <div className="product-card col" key={product._id}>
+                  <div>
+                    <img src={product.imageCover} alt={product.title} />
+                  </div>
 
-        </Container>
+                  <div className="product-info d-flex justify-content-between align-items-center">
+                    <p className="product-title">
+                      {product.title.split(" ").slice(0, 2).join(" ")}
+                    </p>
+                    <div className="d-flex align-items-center gap-1">
+                      <i className="fa-solid fa-star "></i>{" "}
+                      <div className="product-rating">
+                        {product.ratingsAverage}
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="product-category">
+                    <p>{product.category?.name}</p>
+                  </div>
+
+                  <div className="brand">
+                    <p>{product.brand?.name}</p>
+                  </div>
+
+                  <div className="product-content d-flex justify-content-between align-items-center">
+                    <button className="btn btn-primary">Add To Cart</button>
+
+                    <p className="product-price">{product.price}$</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
