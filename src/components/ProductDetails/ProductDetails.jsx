366d854828538db70f55678541aa540709e3929a
HazemAhmedSaad <zozahmed939@gmail.com>
2026-02-28 07:19:59 +0200

    edit in product details page

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -27,6 +27,7 @@ export default function ProductDetails() {
   });
 
   const product = data?.data?.data;
+  console.log(product);
 
   return (
     <div className="container py-5 mt-5 min-vh-100">
@@ -34,7 +35,7 @@ export default function ProductDetails() {
         <ProductDetailsSkeleton />
       ) : (
         <div className="row g-5">
-          <div className="col-md-5">
+          <div className="col-md-5 col-lg-4">
             <Swiper
               navigation
               thumbs={{ swiper: thumbsSwiper }}
@@ -69,36 +70,73 @@ export default function ProductDetails() {
               ))}
             </Swiper>
 
-              <Swiper
-                onSwiper={setThumbsSwiper}
-                slidesPerView={4}
-                spaceBetween={10}
-                centeredSlides={true}
-                pagination={{
-                  clickable: true,
-                }}
-                modules={[Pagination]}
-                className="mt-3 py-2 "
-              >
-                {product?.images?.map((img, index) => (
-                  <SwiperSlide key={index}>
-                    <img
-                      src={img}
-                      onClick={() => {
-                        setActiveIndex(index);
-                        mainSwiper?.slideTo(index);
-                      }}
-                      className={`w-100 rounded thumb-img ${
-                        activeIndex === index ? "active-thumb" : ""
-                      }`}
-                    />
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </div>
+            <Swiper
+              onSwiper={setThumbsSwiper}
+              slidesPerView={4}
+              spaceBetween={10}
+              centeredSlides={true}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Pagination]}
+              className="mt-3 py-2 "
+            >
+              {product?.images?.map((img, index) => (
+                <SwiperSlide key={index}>
+                  <img
+                    src={img}
+                    onClick={() => {
+                      setActiveIndex(index);
+                      mainSwiper?.slideTo(index);
+                    }}
+                    className={`w-100 rounded thumb-img ${
+                      activeIndex === index ? "active-thumb" : ""
+                    }`}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
 
-          <div className="col-md-7">
-            <h3>{product?.title}</h3>
+          <div className="col-md-7 col-lg-8">
+            <div className="badges-info my-3 d-flex gap-2">
+              <span className="badge bg-success">
+                {product?.category?.name}
+              </span>
+              <span className="badge bg-warning">
+                {product?.subcategory?.[0].name}
+              </span>
+              <span className="badge bg-danger">{product?.brand?.name}</span>
+            </div>
+            <h3 className="product-details-title">{product?.title}</h3>
+            <div className="product-rating d-flex align-items-center gap-2 my-2">
+              {Array.from({ length: 5 }, (_, index) => {
+                const rating = product?.ratingsAverage || 0;
+                if (rating >= index + 1) {
+                  return (
+                    <span key={index}>
+                      <i className="fa-solid fa-star fa-sm"></i>
+                    </span>
+                  );
+                } else if (rating >= index + 0.5) {
+                  return (
+                    <span key={index}>
+                      <i className="fa-solid fa-star-half-stroke fa-"></i>
+                    </span>
+                  );
+                } else {
+                  return (
+                    <span key={index} >
+                      <i className="fa-regular fa-star fa-sm"></i>
+                    </span>
+                  );
+                }
+              })}
+              <span className="rating-count">
+                ({product?.ratingsAverage} from {product?.ratingsQuantity}{" "}
+                reviews)
+              </span>
+            </div>
             <h4 className="text-success">{product?.price} EGP</h4>
             <button className="btn btn-dark w-100 mt-3">Add To Cart</button>
           </div>
